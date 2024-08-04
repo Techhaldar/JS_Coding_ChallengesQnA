7bb68d53cac12fc48cb739a9f4cceac0d8388cdb
Ratin <ratinhaldar123@gmail.com>
2024-08-04 21:08:45 +0530

    Add factorial function to compute factorial of a non-negative integer

--- 01_JS_basics/10_Factorial/factorial.js
+++ 01_JS_basics/10_Factorial/factorial.js
@@ -7,4 +7,23 @@
 // It should return the factorial of the given integer.
 
 // ********************** Answer-10 ***********************
+function factorial(num) {
+    if (num <= 0) {
+        console.log("Not a non-negative number");
+    }
+    let fact = 1;
+    let steps = `${num}! = `
+    for (let i = 1; i <= num; i++) {
+        fact = fact * i;
+        if (i < num) {
+            steps = steps + `${i} x `
+        } else {
+            steps = steps + `${i} `
+        }
+    }
+    fact *= 1;
+    steps = steps + `= ${fact}`
+    return steps;
+}
 
+console.log(factorial(prompt("enter the number")));
