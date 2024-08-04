6bad9d2270fc5fe6e1c5270adbcf34b6159fabe7
Ratin <ratinhaldar123@gmail.com>
2024-08-04 10:32:48 +0530

    Add reverseString function to reverse a given string

---
+++ 01_JS_basics/09 reverseString/reverseString.js
@@ -0,0 +1,14 @@
+// Question:
+// Write a JavaScript function called reverseString that takes a string as an argument and returns the reversed version of the string.
+
+// Requirements:
+// The function should be named reverseString.
+// It should accept a single parameter, which is the string to be reversed.
+// It should return the reversed version of the string.
+
+function reverseString(str){
+    let reversed = str.split('').reverse().join('');
+    return reversed
+}
+
+console.log(reverseString(prompt("enter the string:")))
