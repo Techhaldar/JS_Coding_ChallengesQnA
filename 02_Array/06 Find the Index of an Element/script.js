7961d077917ee6397566095ed1b50dc481e1aca5
Ratin <ratinhaldar123@gmail.com>
2024-08-13 12:29:57 +0530

    Find the Index of an Element

---
+++ 02_Array/06 Find the Index of an Element/script.js
@@ -0,0 +1,29 @@
+//  ******************** Question-06 *********************
+// Question:
+// Write a function that finds the index of the first occurrence of a given element in an array.
+
+// Requirements:
+
+// Input: An array and an element to search for.
+// Output: The index of the element in the array.
+// Example Usage:
+// console.log(findIndex([1, 2, 3, 4], 3)); // Output: 2
+
+// ********************** Answer-06 ***********************
+
+function idxElement() {
+    const UserInput = document.querySelector('#UserInput').value;
+    const ElementToFind = document.querySelector('#ElementToFind').value;
+
+    const arr = UserInput.split(' ').map(Number);
+
+
+    const idx = findElement(arr, Number(ElementToFind));
+    document.querySelector('#result').innerText = `index of ${idx}`;
+}
+
+function findElement(arr, element) {
+    return arr.indexOf(element);
+}
+
+console.log(findElement([1, 2, 3, 4, 5], 5));
