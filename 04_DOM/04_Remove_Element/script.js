6ff84ff8158d58a35b3e373449507da726b5d6fb
Ratin <ratinhaldar123@gmail.com>
2024-08-19 13:30:04 +0530

    Remove Element

---
+++ 04_DOM/04_Remove_Element/script.js
@@ -0,0 +1,28 @@
+//  ******************** Question-04 *********************
+
+// Remove Element
+
+// Question: Write a function removeElement that removes an element from the DOM based on its id.
+// Requirements: The function should take one argument: the id of the element to be removed.
+
+// Example Usage:
+/* <div id="toRemove">This will be removed</div>
+<script>
+    removeElement('toRemove');
+</script> */
+
+// ********************** Answer-04 ***********************
+
+function removeElement(id){
+    const toRemove = document.getElementById(id);
+    toRemove.remove();
+}
+
+// removeElement('toRemove');
+
+let button = document.getElementById("button")
+button.addEventListener('click', () => {
+     removeElement('toRemove');
+ })
+
+ 
