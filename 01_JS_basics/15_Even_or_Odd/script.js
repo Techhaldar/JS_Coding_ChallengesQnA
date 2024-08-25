766d7a62955ba6c7266754e9cb58772232ef6f1a
Ratin <ratinhaldar123@gmail.com>
2024-08-25 23:35:58 +0530

    add Even or odd code

--- 01_JS_basics/15_Even_or_Odd/script.js
+++ 01_JS_basics/15_Even_or_Odd/script.js
@@ -13,16 +13,37 @@
 //   console.log(even_or_odd(-3)); // 'Odd'
 
 // ********************** Answer-15 ***********************
-const result = document.querySelector(".box1");
-const button = document.querySelector(".button");
+// const result = document.querySelector(".box1");
+// const button = document.querySelector(".button");
 
-button.addEventListener("click", () => {
-    const input = document.querySelector("input").value;
-    if (input % 2 === 0) {
-        result.innerHTML = `Number is Even`
-    } else {
-        result.innerHTML = `Number is Odd`;
-    }
-})
+// button.addEventListener("click", () => {
+//     const input = document.querySelector("input").value;
+//     if (input % 2 === 0) {
+//         result.innerHTML = `Number is Even`
+//     } else {
+//         result.innerHTML = `Number is Odd`;
+//     }
+// })
+
+
+// X ********************************** X *********************************** X
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// *************** Method 1 *****************
+// const even_or_odd = Number => {
+//     if(Number % 2 === 0){
+//         return `Even number`;
+//     }else{
+//         return `Odd number`;
+//     }
+// }
+// console.log(even_or_odd(15))
+
+// ***************** Method 2 *******************
+
+const even_or_odd = Number => {
+    return Number % 2 === 0 ? 'Even' : 'Odd';
+}
+console.log(even_or_odd(11));
 
 
