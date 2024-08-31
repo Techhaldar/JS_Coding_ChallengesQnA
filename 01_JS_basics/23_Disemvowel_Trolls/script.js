2452bdf6d6ae77164b976f2604aa0ffc526e793a
Ratin <ratinhaldar123@gmail.com>
2024-08-31 20:45:50 +0530

    Find the Odd Int

--- 01_JS_basics/23_Disemvowel_Trolls/script.js
+++ 01_JS_basics/23_Disemvowel_Trolls/script.js
@@ -1,4 +1,4 @@
-//  ******************** Question - 22 *********************
+//  ******************** Question - 23 *********************
 // Trolls are attacking your comment section! A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat. Your task is to write a function that takes a string and returns a new string with all vowels (a, e, i, o, u) removed.
 
 // const disemvowel = str => {
@@ -6,7 +6,7 @@
 //   };
 //   console.log(disemvowel('This website is for losers LOL!')); // 'Ths wbst s fr lsrs LL!'
 
-// ********************** Answer - 22 ***********************
+// ********************** Answer - 23 ***********************
 
 const disemvowel = str => {
     return str.replace(/[aeiou]/ig, '');
