2b0c19964881c885872919704167516257dfd9f5
Ratin <ratinhaldar123@gmail.com>
2024-09-01 19:57:56 +0530

    Square Every Digit

---
+++ 01_JS_basics/27_Square_Every_Digit/Square_Every_Digit.js
@@ -0,0 +1,32 @@
+//  ******************** Question - 27 *********************
+
+// Given an integer, your task is to square every digit of it and concatenate them to produce a new integer.
+
+// const squareDigits = num => {
+//     // Your solution
+//   };
+
+//   console.log(squareDigits(2112)); // 4114
+//   console.log(squareDigits(3212)); // 9414
+//   console.log(squareDigits(9159)); // 8112581
+
+// ********************** Answer - 27 ***********************
+
+// mathod-1
+// const squareDigits = number => {
+//     let count = ""
+//     let num = number.toString().split('')
+//     // console.log(num)
+//     for (const digit of num) {
+//         count += digit * digit;
+//     }
+//     console.log(count);
+// }
+
+// squareDigits(2552);
+
+// mathod-2
+
+const squareDigits = num => {
+    
+};
