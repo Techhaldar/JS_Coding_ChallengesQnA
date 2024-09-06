d064ff7a6091fbe74461505ddd4ac56af7dd3c10
Ratin <ratinhaldar123@gmail.com>
2024-09-06 20:51:45 +0530

    You're a Square!

---
+++ 01_JS_basics/29_You're_a_Square!/script.js
@@ -0,0 +1,22 @@
+//  ******************** Question - 29 *********************
+// You're a Square!
+// Write a function that given an integer, checks to see if it is a square number. A square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+// const isSquare = n => {
+//     // Your solution
+//   };
+
+//   console.log(isSquare(0)); // true
+//   console.log(isSquare(4)); // true
+//   console.log(isSquare(25)); // true
+//   console.log(isSquare(3)); // false
+//   console.log(isSquare(93)); // false
+//   console.log(isSquare(-1)); // false
+
+// ********************** Answer - 29 ***********************
+
+const isSquare = n => {
+  return  Math.sqrt(n) % 1 === 0 ;
+};
+
+console.log(isSquare(4))
