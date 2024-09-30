f8ed651c747d91cea881dac67ca343bdca10affb
Ratin <ratinhaldar123@gmail.com>
2024-09-30 22:21:13 +0530

    Sum of Two Numbers

---
+++ 01_JS_basics/S05/L-1/script.js
@@ -0,0 +1,15 @@
+// 1. Sum of Two Numbers
+// Problem:
+// Write a function that takes two numbers as input and returns their sum.
+
+// Explanation:
+// The function sum takes two numbers as parameters, a and b.
+// It simply adds them together using the + operator and returns the result.
+
+function Sum(a, b) {
+  console.log("Sum of number:",  a + b);
+}
+
+Sum(10, 20) // OutPut 30
+
+
